73545707ba709a84506900bcb0af96a56ce437df
Ashutosh Mohanta <mohantaashutosh@yahoo.com>
2022-06-06 06:05:27 +0530

    change order of #define

--- mT.cpp
+++ mT.cpp
@@ -4,18 +4,19 @@ using namespace std;
 //Email  => mohantaashutosh@yahoo.com
 //Template Variables
 //START
+#define rep(i,n) for(int i=0;i<(n);++i)
+#define per(i,n) for(int i=(n-1);i>=0;--i)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define vei(y,x) vvi((y),vi(x))
+#define vel(y,x) vvl((y),vl(x));
 typedef long long ll;
 typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vll;
 typedef long double ld;
 typedef unsigned long long ull;
-#define INF 2147483647
-#define rep(i,n) for(int i=0;i<n;++i)
-#define per(i,n) for(int i=n-1;i>=0;--i)
-#define pb push_back
-#define mp make_pair
-#define all(x) (x).begin(), (x).end()
-
 //END
 //
 
@@ -26,6 +27,11 @@ istream& operator>>(istream& is,vector<T>&v)
 {
     for(auto &i:v)is>>i;
     return is;
+}
+template<class T1,class T2>
+ostream& operator<<(ostream &os,pair<T1,T2> p){
+    os<<'{'<<p.first<<' '<<p.second<<'}';
+    return os;
 }
 template<class T>
 ostream& operator<<(ostream& os,const vector<T>&v)
@@ -50,7 +56,9 @@ string operator*(string s,int a){
 }
 //END
 //
+//Data Structures
 
+//
 
 void solve(){
 }
